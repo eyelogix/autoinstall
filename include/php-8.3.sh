1e3b3479776f127396d1b08503d6e62aa3a40ae5
eyelogix <51819706+eyelogix@users.noreply.github.com>
2024-04-01 00:47:33 +0530

    Update php-8.3.sh

--- include/php-8.3.sh
+++ include/php-8.3.sh
@@ -150,7 +150,7 @@ Install_PHP83() {
   sed -i 's@^post_max_size.*@post_max_size = 200M@' ${php_install_dir}/etc/php.ini
   sed -i 's@^upload_max_filesize.*@upload_max_filesize = 200M@' ${php_install_dir}/etc/php.ini
   sed -i 's@^max_execution_time.*@max_execution_time = 600@' ${php_install_dir}/etc/php.ini
-  sed -i 's@^max_execution_time.*@max_input_time = 600@' ${php_install_dir}/etc/php.ini
+  sed -i 's@^max_input_time.*@max_input_time = 600@' ${php_install_dir}/etc/php.ini
   sed -i 's@^;realpath_cache_size.*@realpath_cache_size = 2M@' ${php_install_dir}/etc/php.ini
   sed -i 's@^disable_functions.*@disable_functions = passthru,exec,system,chroot,chgrp,chown,shell_exec,proc_open,proc_get_status,ini_alter,ini_restore,dl,readlink,symlink,popepassthru,stream_socket_server,fsocket,popen@' ${php_install_dir}/etc/php.ini
   [ -e /usr/sbin/sendmail ] && sed -i 's@^;sendmail_path.*@sendmail_path = /usr/sbin/sendmail -t -i@' ${php_install_dir}/etc/php.ini
