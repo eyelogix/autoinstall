e388cf64fd25ddd1878f1a834f7dbaa5661c2e81
eyelogix <51819706+eyelogix@users.noreply.github.com>
2024-03-31 22:50:47 +0530

    Update init_Debian.sh

--- include/init_Debian.sh
+++ include/init_Debian.sh
@@ -67,7 +67,7 @@ fs.inotify.max_user_instances = 8192
 net.ipv4.tcp_syncookies = 1
 net.ipv4.tcp_fin_timeout = 30
 net.ipv4.tcp_tw_reuse = 1
-net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.ip_local_port_range = 10000 65000
 net.ipv4.tcp_max_syn_backlog = 16384
 net.ipv4.tcp_max_tw_buckets = 6000
 net.ipv4.route.gc_timeout = 100
@@ -77,6 +77,11 @@ net.core.somaxconn = 32768
 net.core.netdev_max_backlog = 32768
 net.ipv4.tcp_timestamps = 0
 net.ipv4.tcp_max_orphans = 32768
+# ---- For Max Threads Limit -----
+kernel.pid_max = 300000
+kernel.threads-max = 300000
+vm.max_map_count = 300000
+# --------------------------------
 EOF
 sysctl -p
 
